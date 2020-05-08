8dcf271479e71e53feac35dc81876f5699873fd0
Jason <jasylwong@gmail.com>
2020-05-08 23:01:39 +0100

    update statement;

--- lib/statement.rb
+++ lib/statement.rb
@@ -7,7 +7,7 @@ class Statement
   end
 
   def print_out
-    print no_transactions? ? HEADER : [HEADER, format_transactions].join("\n")
+    puts no_transactions? ? HEADER : [HEADER, format_transactions].join("\n")
   end
 
   private 
